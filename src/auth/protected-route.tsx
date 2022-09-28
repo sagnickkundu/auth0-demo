3062b772b8028f442ce1c79c0f2ac5908e710532
Sagnick Kundu <54092946+sagnickkundu@users.noreply.github.com>
2022-09-28 08:15:23 +0530

    Add files via upload

---
+++ src/auth/protected-route.tsx
@@ -0,0 +1,21 @@
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+import React, { ComponentType } from "react";
+
+interface ProtectedRouteProps {
+  component: ComponentType;
+}
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+  component,
+}) => {
+  const Component = withAuthenticationRequired(component, {
+    onRedirecting: () => (
+      <div className="page-layout">
+        Loading...
+      </div>
+    ),
+  });
+
+  return <Component />;
+};
+
+export default ProtectedRoute;
